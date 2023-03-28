c9f76e607145ae78f444e58d460bfad7bd7dd92f
João Vitor Maia <maia.tostring@gmail.com>
2023-03-28 17:35:56 -0300

    Adding dark theme as default

--- gordot/__main__.py
+++ gordot/__main__.py
@@ -1,9 +1,12 @@
 import sys
+import qdarktheme
 
 from PyQt5.QtWidgets import QApplication, QWidget
 from gordot.views.main_window import MainWindow
 
 app = QApplication(sys.argv)
+
+qdarktheme.setup_theme()
 
 window = MainWindow()
 
